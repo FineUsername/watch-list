bd3ccf4f8df0894e2f50d942ea008280193fc17a
Dmitri_Prudnikov <tertbot02@gmail.com>
2022-05-13 20:06:41 +0300

    Remove debug information

--- src/main/java/com/mygroup/watchlist/WatchlistApplication.java
+++ src/main/java/com/mygroup/watchlist/WatchlistApplication.java
@@ -2,10 +2,8 @@ package com.mygroup.watchlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class WatchlistApplication {
 
   public static void main(String[] args) {
